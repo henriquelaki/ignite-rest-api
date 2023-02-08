b8fd06328fa02e347c028ea03bf84f7102869f74
Henrique Laki <henrique.laki@gmail.com>
2023-02-08 02:15:12 -0300

    Creating transaction tables

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async (request, reply) => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions').select('*')
+
+  return transactions
 })
 
 app
