0b8b524a69dd2a6a249ea99ebe69d83ad4f8ba5f
Henrique Laki <henrique.laki@gmail.com>
2023-02-08 21:57:36 -0300

    Adding e2e tests for transactions domain

--- tests/transactions.spec.ts
+++ tests/transactions.spec.ts
@@ -1,7 +1,15 @@
-import { beforeAll, afterAll, afterEach, describe, it, expect } from 'vitest'
+import {
+  beforeAll,
+  afterAll,
+  afterEach,
+  describe,
+  it,
+  expect,
+  beforeEach,
+} from 'vitest'
+import { execSync } from 'node:child_process'
 import request from 'supertest'
 import { app } from '../src/app'
-import { knex } from '../src/database'
 import { randomUUID } from 'node:crypto'
 
 const testId = randomUUID()
@@ -12,8 +20,12 @@ describe('Transaction routes', () => {
     return app.ready()
   })
 
+  beforeEach(async () => {
+    execSync('npm run knex -- migrate:latest')
+  })
+
   afterEach(async () => {
-    await knex('transactions').delete().where('title', title)
+    execSync('npm run knex -- migrate:rollback --all')
   })
 
   afterAll(() => {
@@ -55,5 +67,146 @@ describe('Transaction routes', () => {
 
   it('should not be able to get a list of transactions without authentication', async () => {
     await request(app.server).get('/transactions').expect(401)
+  })
+
+  it('should be able to get an unique transaction with authentication', async () => {
+    const firstTransaction = {
+      title,
+      amount: 100,
+      type: 'credit',
+    }
+
+    const secondTransaction = {
+      title: `Other ${title}`,
+      amount: 200,
+      type: 'credit',
+    }
+
+    const createFirstTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(firstTransaction)
+
+    const createSecondTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(secondTransaction)
+
+    await request(app.server).post('/transactions').send(secondTransaction)
+
+    const cookiesFromFirstTransaction =
+      createFirstTransactionResponse.get('Set-Cookie')
+
+    const cookiesFromSecondTransaction =
+      createSecondTransactionResponse.get('Set-Cookie')
+
+    const listOfTransactionsFromFirstSession = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookiesFromFirstTransaction)
+
+    const firstTransactionId =
+      listOfTransactionsFromFirstSession.body.transactions[0].id
+
+    const listOfTransactionsFromSecondSession = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookiesFromSecondTransaction)
+
+    const secondTransactionId =
+      listOfTransactionsFromSecondSession.body.transactions[0].id
+
+    const getTransactionFromFirstSessionResponse = await request(app.server)
+      .get(`/transactions/${firstTransactionId}`)
+      .set('Cookie', cookiesFromFirstTransaction)
+      .expect(200)
+
+    expect(getTransactionFromFirstSessionResponse.body?.transaction).toEqual(
+      expect.objectContaining({ title, amount: 100 }),
+    )
+
+    const getTransactionFromSecondSessionResponse = await request(app.server)
+      .get(`/transactions/${secondTransactionId}`)
+      .set('Cookie', cookiesFromSecondTransaction)
+      .expect(200)
+
+    expect(getTransactionFromSecondSessionResponse.body?.transaction).toEqual(
+      expect.objectContaining({ title: `Other ${title}`, amount: 200 }),
+    )
+  })
+
+  it('should not be able to get an unique transaction without authentication', async () => {
+    await request(app.server).get(`/transactions/${randomUUID()}`).expect(401)
+  })
+
+  it('should not be able to get an unique transaction that does not exists', async () => {
+    const transaction = {
+      title,
+      amount: 100,
+      type: 'credit',
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(transaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await request(app.server)
+      .get(`/transactions/${randomUUID()}`)
+      .set('Cookie', cookies)
+      .expect(404)
+  })
+
+  it('should not be able to get an unique transaction that does not belongs to the user', async () => {
+    const transaction = {
+      title,
+      amount: 100,
+      type: 'credit',
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(transaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listOfTransactions = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    const transactionId = listOfTransactions.body.transactions[0].id
+
+    const transactionSessionId = createTransactionResponse
+      .get('Set-Cookie')[0]
+      .split(';')[0]
+      .split('=')[1]
+
+    cookies[0] = cookies[0].replace(transactionSessionId, randomUUID())
+
+    await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(404)
+  })
+  it('should be able to get a summary of transactions with authentication', async () => {
+    const transaction = {
+      title,
+      amount: 100,
+      type: 'credit',
+    }
+
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send(transaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const summaryResponse = await request(app.server)
+      .get('/transactions/summary')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(summaryResponse.body?.summary).toEqual({ amount: 100 })
+  })
+
+  it('should not be able to get a summary of transactions without authentication', async () => {
+    await request(app.server).get('/transactions/summary').expect(401)
   })
 })
